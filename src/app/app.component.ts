81e7ec80bf9d0ee227366ea61d5b59d5c5e0f010
Konijima <mathieugermain1991@gmail.com>
2023-04-20 07:18:22 -0400

    Fetch service data  in app component and test chat stream completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { OpenAI } from './app.module';
+import { ModelsService } from './services/models.service';
+import { FineTunesService } from './services/fine-tunes.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  text: string = '';
 
-  constructor() {
-    console.log(window.OpenAI);
+  constructor(
+    private zone: NgZone,
+    private modelService: ModelsService,
+    private fineTunesService: FineTunesService,
+    private filesService: FilesService
+  ) {
+    if (!OpenAI.isApiKeySet()) return;
 
-    window.OpenAI.setApiKey('sk-************************************');
-    window.OpenAI.listModels().then((data) => console.log(data));
+    console.log(
+      'Fetching initial data...',
+      '[models]',
+      '[files]',
+      '[fine-tunes]'
+    );
+    Promise.allSettled([
+      this.modelService.fetchModels(),
+      this.filesService.fetchFiles(),
+      this.fineTunesService.fetchFineTunes(),
+    ]).then((results) => {
+      console.log(
+        'Initial Fetch Results:',
+        results.map((r) => r.status)
+      );
+    });
+
+    // testing: stream chat completion
+    OpenAI.createStreamChatCompletion(
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a javascript developer.',
+          },
+          {
+            role: 'user',
+            content: 'Show me a tiny code snippet',
+          },
+        ],
+        max_tokens: 50,
+      },
+      (data) =>
+        this.zone.run(() => {
+          this.text += data;
+        })
+    )
+      .then((result) => console.log('Chat Stream Completed', result))
+      .catch((err) => console.error(err));
   }
 }
