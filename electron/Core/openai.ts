96412c09a49cdc1d1211481d61b860a64ac33c50
Konijima <mathieugermain1991@gmail.com>
2023-04-24 01:06:05 -0400

    Wrap function in try-catch block
    This commit wraps a function in a try-catch block to handle any errors that may occur during its execution. This is done to ensure that the app does not crash or behave unexpectedly in case of an error. By catching errors and handling them gracefully, the app can continue to function as expected and provide a better user experience.

--- electron/Core/openai.ts
+++ electron/Core/openai.ts
@@ -14,54 +14,62 @@ const keyPath = join(app.getPath('home'), '.openai-studio-api-key');
 
 // save the api key
 ipcMain.handle('saveApiKey', async (event, apiKey) => {
-  if (apiKey !== '' && apiKey !== undefined) {
-    // create a cipherkey from key
-    const cipherKey = crypto.scryptSync(key, 'salt', 32);
+  try {
+    if (apiKey !== '' && apiKey !== undefined) {
+      // create a cipherkey from key
+      const cipherKey = crypto.scryptSync(key, 'salt', 32);
 
-    const iv = crypto.randomBytes(16); // generate random initialization vector
-    const cipher = crypto.createCipheriv(algorithm, cipherKey, iv); // create cipher object
-    const encrypted = Buffer.concat([cipher.update(apiKey), cipher.final()]); // encrypt data
+      const iv = crypto.randomBytes(16); // generate random initialization vector
+      const cipher = crypto.createCipheriv(algorithm, cipherKey, iv); // create cipher object
+      const encrypted = Buffer.concat([cipher.update(apiKey), cipher.final()]); // encrypt data
 
-    await writeFile(
-      keyPath,
-      `${iv.toString('hex')}:${encrypted.toString('hex')}`,
-      { encoding: 'utf8' }
-    );
-    return;
+      await writeFile(
+        keyPath,
+        `${iv.toString('hex')}:${encrypted.toString('hex')}`,
+        { encoding: 'utf8' }
+      );
+      return;
+    }
+
+    await writeFile(keyPath, '', { encoding: 'utf8' });
+  } catch (err) {
+    console.log(err);
   }
-
-  await writeFile(keyPath, '', { encoding: 'utf8' });
 });
 
 // decrypt the api key
 ipcMain.handle('getApiKey', async () => {
-  // read the file
-  const encrypted = await readFile(keyPath, { encoding: 'utf8' });
+  try {
+    // read the file
+    const encrypted = await readFile(keyPath, { encoding: 'utf8' });
 
-  // if the file is empty return an empty string
-  if (encrypted === '') {
-    throw new Error('No API key found');
+    // if the file is empty return an empty string
+    if (encrypted === '') {
+      throw new Error('No API key found');
+    }
+
+    // get the iv and encrypted data
+    const [iv, encryptedData] = encrypted.split(':');
+
+    // create a cipherkey from key
+    const cipherKey = crypto.scryptSync(key, 'salt', 32);
+
+    // create decipher object
+    const decipher = crypto.createDecipheriv(
+      algorithm,
+      cipherKey,
+      Buffer.from(iv, 'hex')
+    );
+
+    // decrypt data
+    const decrypted = Buffer.concat([
+      decipher.update(Buffer.from(encryptedData, 'hex')),
+      decipher.final(),
+    ]);
+
+    // return the decrypted data
+    return decrypted.toString();
+  } catch (err) {
+    console.log(err);
   }
-
-  // get the iv and encrypted data
-  const [iv, encryptedData] = encrypted.split(':');
-
-  // create a cipherkey from key
-  const cipherKey = crypto.scryptSync(key, 'salt', 32);
-
-  // create decipher object
-  const decipher = crypto.createDecipheriv(
-    algorithm,
-    cipherKey,
-    Buffer.from(iv, 'hex')
-  );
-
-  // decrypt data
-  const decrypted = Buffer.concat([
-    decipher.update(Buffer.from(encryptedData, 'hex')),
-    decipher.final(),
-  ]);
-
-  // return the decrypted data
-  return decrypted.toString();
 });
